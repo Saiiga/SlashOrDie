3d7a589e42e5f783fb8f5762bbfd275915aff85f
AMORAND <adrien.morand@ynov.com>
2022-02-16 15:31:11 +0100

    put placeholder abstract method

---
+++ Impact_game/Assets/Script/Common/Player.cs
@@ -0,0 +1,31 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+    public override void OnDie()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnHit()
+    {
+        throw new System.NotImplementedException();
+    }
+} /*Héritage Entity
+
+attributs:
+Collider hitbox
+Rigibody rigibody
+Sprite sprite
+Animator animator
+List<Bonus> bonus
+float moveSpeed
+
+fonction:
+Attack TryToRecognizeAttack()
+void AddBonus(Bonus)
+void RemoveBonus(Bonus)
+void OnHit() (ajouter le traitement si touché par projectile et bonus)*/
