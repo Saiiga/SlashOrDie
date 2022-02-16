e0353c1e87f6cfae96afa4c30933f81fcd0380a0
AMORAND <adrien.morand@ynov.com>
2022-02-16 15:05:18 +0100

    start player

---
+++ Impact_game/Assets/Script/Common/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+    
+   
+
+
+} /*Héritage Entity
+
+attributs:
+Collider hitbox
+Rigibody rigibody
+Sprite sprite
+Animator animator
+List<Bonus> bonus
+float moveSpeed
+
+fonction:
+Attack TryToRecognizeAttack()
+void AddBonus(Bonus)
+void RemoveBonus(Bonus)
+void OnHit() (ajouter le traitement si touché par projectile et bonus)*/
