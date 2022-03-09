9f2ba87354f3b175253bb3016c7dc1733efd4023
AMORAND <adrien.morand@ynov.com>
2022-03-09 09:57:52 +0100

    player update 1.1

--- Impact_game/Assets/Script/Common/Player.cs
+++ Impact_game/Assets/Script/Common/Player.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class Player : Entity
 {
-    List<Bonus> m_bonus= new List<Bonus>();
     public float m_moveSpeed;
     public int m_HP;
 
@@ -21,14 +20,6 @@ public class Player : Entity
     public void TryToRecognizeAttack()
     {
 
-    }
-    public void AddBonus(Bonus bonus)
-    {
-        m_bonus.Add(bonus);
-    }
-    public void RemoveBonus(Bonus bonus)
-    {
-        m_bonus.Remove(bonus);
     }
     public void AddHP(int HP)
     {
