ce0fc80c1f81d43f53f1d9f1c8a45213b2ef5f62
Adrien Morand <90243392+Affy657@users.noreply.github.com>
2022-03-09 10:04:04 +0100

    Create : base Player (#1)
    * start player
    
    * put placeholder abstract method
    
    * cherry-pick player and sanitize bonus
    
    * create player base 1.1
    
    * player update 1.1
    
    Co-authored-by: AMORAND <adrien.morand@ynov.com>

---
+++ Impact_game/Assets/Script/Common/Player.cs
@@ -0,0 +1,33 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+    public float m_moveSpeed;
+    public int m_HP;
+
+    public override void OnDie()
+    {
+        animator.Play("Death");
+
+    }
+    public override void OnHit()
+    {
+        animator.Play("Hit");
+    }
+    public void TryToRecognizeAttack()
+    {
+
+    }
+    public void AddHP(int HP)
+    {
+        m_HP += HP;
+    }
+    public void RemoveHP(int HP)
+    {
+        m_HP -= HP;
+    }
+} 
+
