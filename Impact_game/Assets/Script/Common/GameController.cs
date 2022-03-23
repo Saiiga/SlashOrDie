a96a0f784036baaa831466d100af16e0eba6b652
AlexAmemoutou <alex.amemoutou@gmail.com>
2022-03-23 18:27:21 +0100

    Add : GameController which will control all usefull data Utils with commun usefull function Projectil's logic of how recognize, generate, show, change and destroy input which be press to destroy the projectil

---
+++ Impact_game/Assets/Script/Common/GameController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UnityEngine.InputSystem.DualShock;
+
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem;
+
+public static class GameController
+{
+    //Point of where the projectils should be allow to being destroy
+    [SerializeField] public static Vector2 pointAttackAvailable = new Vector2(0,0);
+    private static DualShockGamepad gamepad;
+    private static Keyboard keyboard;
+    private static ButtonControl[] authorisedKeybordCodes;
+    private static ButtonControl[] authorisedControllerCodes;
+
+    public static DualShockGamepad GetDualShockGamepad()
+    {
+        if(gamepad == null)
+            gamepad = DualShockGamepad.current;
+        return gamepad;
+
+    }
+
+    public static Keyboard GetKeyboard()
+    {
+        if (keyboard == null)
+            keyboard = Keyboard.current;
+        return keyboard;
+
+    }
+
+    private static ButtonControl[] GetAuthorisedKeybordCodes()
+    {
+        if(authorisedKeybordCodes == null)
+        {
+            GetKeyboard();
+            authorisedKeybordCodes = new KeyControl[]
+            {
+                keyboard.wKey,keyboard.aKey,keyboard.sKey,keyboard.dKey,
+                keyboard.iKey,keyboard.jKey,keyboard.kKey,keyboard.lKey,
+                keyboard.fKey
+            };
+        }
+        return authorisedKeybordCodes;
+    }
+
+    private static ButtonControl[] GetAuthorisedControllerCodes()
+    {
+        if (authorisedControllerCodes == null)
+        {
+            GetDualShockGamepad();
+            authorisedControllerCodes = new ButtonControl[]
+            {
+                gamepad.bButton,gamepad.xButton,gamepad.yButton,gamepad.rightShoulder,gamepad.leftShoulder,
+                gamepad.buttonEast,gamepad.buttonNorth,gamepad.buttonWest,gamepad.buttonSouth,
+            };
+        }
+        return authorisedControllerCodes;
+    }
+
+    public static ButtonControl[] GetAuthorizedCodes()
+    {
+        return GetEnvironnement() switch
+        {
+            0 => GetAuthorisedKeybordCodes(),
+            1 => GetAuthorisedControllerCodes(),
+            _ => throw new System.Exception("Can't found environnement."),
+        };
+    }
+
+    public static int GetEnvironnement()
+    {
+        return 0;
+    }
+
+
+}
--- Impact_game/Assets/Script/Projectils/Projectil.cs
+++ Impact_game/Assets/Script/Projectils/Projectil.cs
@@ -1,12 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class Projectil : Entity
 {
-    public ProjectilType projectilType;
+    [SerializeField] private int m_difficulty;
+    [SerializeField] private TextMesh textMeshToShow;
+    [SerializeField] private int currentIndex = 0;
+
+    [SerializeField] public List<ButtonControl> inputsToDo;
+    [SerializeField] public ProjectilType projectilType;
+
+    public void Start()
+    {
+        inputsToDo = new List<ButtonControl>();
+
+        ButtonControl[] availableInput = GameController.GetAuthorizedCodes();
+        //Depend of projectil's difficulty, allow to use all possible input or not
+        int maxDifferentInputType = (int)(m_difficulty > 4 ? availableInput.Length - 1 : System.Math.Ceiling(availableInput.Length / 2d));
+
+        for(int i=0; i<m_difficulty; i++)
+        {
+            inputsToDo.Add(availableInput[Random.Range(0, maxDifferentInputType)]);
+        }
+        ChangeInputToShow();
+    }
+
+    public void Update()
+    {
+        if(IsDrestroyable())
+        {
+            CheckInput();
+        }
+    }
+
+    public bool IsDrestroyable()
+    {
+        return transform.position.x <= GameController.pointAttackAvailable.x;
+      
+    }
+
+    public override void OnDie()
+    {
+        animator.Play("Death");
+        Destroy(this.gameObject);
+    }
+
+    public override void OnHit()
+    {
+        OnDie();
+    }
+
     public void OnTriggerEnter2D(Collider2D collider)
     {
+
         Player player = collider.GetComponent<Player>();
 
         if (player != null)
@@ -15,15 +63,27 @@ public class Projectil : Entity
             OnHit();
         }
     }
-
-    public override void OnDie()
+    private void CheckInput()
     {
-        animator.Play("Death");
-        Destroy(this);
+        if (inputsToDo[currentIndex].wasPressedThisFrame)
+        {
+            currentIndex++;
+            if (currentIndex == inputsToDo.Count)
+            {
+                OnDie();
+                return;
+            }
+            ChangeInputToShow();
+        }
+        else if (GameController.GetKeyboard().anyKey.wasPressedThisFrame)
+        {
+            currentIndex = 0;
+            ChangeInputToShow();
+        }
+    }
+    private void ChangeInputToShow()
+    {
+        textMeshToShow.text = Utils.QwertyToAzerty(inputsToDo[currentIndex].name.ToUpper());
     }
 
-    public override void OnHit()
-    {
-        OnDie();
-    }
 }
