ae29c4ce33be55bf8a7695d1e304ad4781175c10
Adrien Morand <90243392+Affy657@users.noreply.github.com>
2022-03-23 15:26:20 +0100

    Add jump feature (#4)
    * start player
    
    * put placeholder abstract method
    
    * cherry-pick player and sanitize bonus
    
    * create player base 1.1
    
    * player update 1.1
    
    * player removeHp finish
    
    * player jump and isGrounded
    
    * netoiyage
    
    * fixe
    
    * remove useless Bonus file
    
    Co-authored-by: AMORAND <adrien.morand@ynov.com>
    Co-authored-by: Alex Amemoutou <mr.say6@gmail.com>

--- Impact_game/Assets/Script/Common/Player.cs
+++ Impact_game/Assets/Script/Common/Player.cs
@@ -7,25 +7,49 @@ public class Player : Entity
 {
     public float m_moveSpeed;
     public int m_HP;
+    public float jumpForce ;
+    [SerializeField] private bool isJumping;
+    [SerializeField] private bool isGrounded;
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+
+
+    public void Update()
+    {
+            isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+        if (isJumping == true)
+        {
+            rigibody.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
 
     public override void OnDie()
     {
         animator.Play("Death");
 
     }
+    
     public override void OnHit()
     {
         animator.Play("Hit");
         RemoveHP(1);
     }
+    
     public void TryToRecognizeAttack()
     {
 
     }
+    
     public void AddHP(int HP)
     {
         m_HP += HP;
     }
+    
     public void RemoveHP(int HP)
     {
         m_HP -= HP;
@@ -35,4 +59,3 @@ public class Player : Entity
         }
     }
 } 
-
