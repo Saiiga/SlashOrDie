974ad66cf56b71e4ff8afe7110ade2ec43f0bcac
AMORAND <adrien.morand@ynov.com>
2022-03-23 14:52:29 +0100

    craet BonusType and add SightDown,SightUp feature

--- Impact_game/Assets/Script/Abstract/Bonus.cs
+++ Impact_game/Assets/Script/Abstract/Bonus.cs
@@ -2,12 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Bonus : Entity
+public class Bonus : Entity
 {
-
+    float cameraSize = 1.7f;
+    [SerializeField] BonusType bonusType;
     public void Action(Player player)
     {
+      
             
+    }
+
+    public override void OnDie()
+    {
+
+    }
+
+    public override void OnHit()
+    {
+
+    }
+
+    public void SightDown(Player player)
+    {
+        GetComponent<Camera>().orthographicSize = cameraSize - 0.4f;
+        //timer
+        GetComponent<Camera>().orthographicSize = cameraSize + 0.4f;
+    }
+    public void SightUp(Player player)
+    {
+        GetComponent<Camera>().orthographicSize = cameraSize + 0.4f;
+        //timer
+        GetComponent<Camera>().orthographicSize = cameraSize - 0.4f;
+    }
+
+    public void Shild(Player player)
+    {
+
     }
 
 }
