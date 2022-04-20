1be59372e39a194ce1d03d8f0c35900e026fce7f
AlexAmemoutou <alex.amemoutou@gmail.com>
2022-04-20 16:29:03 +0200

    Done Sight bonus and malus

---
+++ Impact_game/Assets/Script/Common/PowerObjects/SightDown.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SightDown : PowerObject
+{
+    [SerializeField] float bonusSightEffect;
+    [SerializeField] float speedEffect;
+    private float initialZoom;
+    private float targetZoom;
+    public override void OnHit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Action(Player player)
+    {
+        StartCoroutine(BonusSightUpCoroutine());
+        isActivated = true;
+    }
+
+    public void Start()
+    {
+        initialZoom = Camera.main.orthographicSize;
+        targetZoom = Camera.main.orthographicSize * (1 - bonusSightEffect);
+    }
+
+    public void Update()
+    {
+    }
+
+    IEnumerator BonusSightUpCoroutine()
+    {
+        Camera.main.orthographicSize = targetZoom; 
+        Camera.main.transform.position = new Vector3(0, -1.1f, Camera.main.transform.position.z);
+        //camera.orthographicSize = camera.orthographicSize * zoomFactor;
+        yield return new WaitForSeconds(bonusTime);
+        //camera.orthographicSize = camera.orthographicSize / zoomFactor;
+        Camera.main.orthographicSize = initialZoom;
+        Camera.main.transform.position = new Vector3(0, -.2f, Camera.main.transform.position.z);
+
+    }
+
+}
---
+++ Impact_game/Assets/Script/Common/PowerObjects/SightUp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SightUp : PowerObject
+{
+    [SerializeField] float bonusSightEffect;
+    [SerializeField] float speedEffect;
+    private float initialZoom;
+    private float targetZoom;
+    public override void OnHit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Action(Player player)
+    {
+        StartCoroutine(BonusSightUpCoroutine());
+        isActivated = true;
+    }
+
+    public void Start()
+    {
+        initialZoom = Camera.main.orthographicSize;
+        targetZoom = Camera.main.orthographicSize * (1 + bonusSightEffect);
+    }
+
+    public void Update()
+    {
+    }
+
+    IEnumerator BonusSightUpCoroutine()
+    {
+        Camera.main.orthographicSize = targetZoom;
+        //camera.orthographicSize = camera.orthographicSize * zoomFactor;
+        yield return new WaitForSeconds(bonusTime);
+        //camera.orthographicSize = camera.orthographicSize / zoomFactor;
+        Camera.main.orthographicSize = initialZoom;
+
+    }
+
+}
